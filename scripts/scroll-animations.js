af04509c49f0337829f1d769073c2ac17e0e9e91
santilordi <santilordi569@gmail.com>
2025-03-07 16:40:28 -0300

    Scroll animation

---
+++ scripts/scroll-animations.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+    
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
